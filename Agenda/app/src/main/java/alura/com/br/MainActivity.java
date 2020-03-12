97d596412b8f22987f49ff31a803a483a32c7149
Ygor-Sousa <56745712+ygor-sousa@users.noreply.github.com>
2020-03-12 20:37:43 -0300

    Agenda 2

--- Agenda/app/src/main/java/alura/com/br/MainActivity.java
+++ Agenda/app/src/main/java/alura/com/br/MainActivity.java
@@ -1,6 +1,18 @@
 package alura.com.br;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView aluno = new TextView(this);
+        aluno.setText("Ygor Santos");
+        setContentView(aluno);
+    }
 }
